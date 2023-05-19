7657bf106103c5af14e8d9f47b634b2dad8f556d
= <=>
2023-05-19 23:52:01 +0300

    time2

--- Modules/Api/Http/Controllers/ProductsController.php
+++ Modules/Api/Http/Controllers/ProductsController.php
@@ -141,7 +141,8 @@ class ProductsController extends Controller{
                 'variation_id' => $variationAttribute->variation_id,
                 'percentage_of_rating' => $product->percentage_of_rating,
                 'media' => $medias,
-                'time' => round(($product->distance/1000/60*60) + 5) . ' to ' . round(($product->distance/1000/30*60) + 5) . ' min',
+                'distance' => round($product->distance / 1000 / 5) == 0 ? 5 : round($product->distance / 1000 / 5),,
+                'time' =>  round(($product->distance/1000/60*60) + 5) . ' to ' . round(($product->distance/1000/30*60) + 5) . ' min',
                 'attributesList'=> $attributesList
             ]
         ]);
